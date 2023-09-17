0769351e58716efc4fb943464b7690437aebfaf1
Manmeet567 <manmeet567singh@gmail.com>
2023-09-17 11:22:15 +0530

    Created Home Page

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Main from '../Components/ForHome/Main'
+import Navbar from '../Components/Common/Navbar'
+
+function Home() {
+  return (
+    <div className="home">
+        <Navbar />
+        <Main />
+    </div>
+  )
+}
+
+export default Home
