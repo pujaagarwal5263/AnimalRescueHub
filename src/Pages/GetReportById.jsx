a593ee4951e86c677c290cbea095842e6d4b0850
Manmeet567 <manmeet567singh@gmail.com>
2023-09-24 16:03:48 +0530

    created component for get report by id

---
+++ src/Pages/GetReportById.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Components/Common/Navbar'
+import Main from '../Components/ForGetReportById/Main'
+
+
+function GetReportById() {
+  return (
+    <div>
+        <Navbar />
+        <Main />
+    </div>
+  )
+}
+
+export default GetReportById
