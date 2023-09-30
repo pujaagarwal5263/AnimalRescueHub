dce53b45738e95dfd22a3b2180b83bc35c59c874
Manmeet567 <manmeet567singh@gmail.com>
2023-09-30 16:44:27 +0530

    Added backend link

--- src/Components/ForAdminLogin/Main.jsx
+++ src/Components/ForAdminLogin/Main.jsx
@@ -26,6 +26,7 @@ const Main = () => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
+
         },
         body: JSON.stringify(requestBody),
       });
